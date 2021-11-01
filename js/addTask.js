cde039d2ebd20557801a69c769f7c67c8d629af2
Toma Gela <toma.gela@gmail.com>
2021-11-01 18:09:36 +0100

    urgency added

--- js/addTask.js
+++ js/addTask.js
@@ -1,60 +1,59 @@
  setURL('http://developerakademie.com/smallest_backend_ever');
 
 
- let allTasks = [
-    {
-        'taskTitle': 'Css überarbeiten', 
-        'taskCategory': 'Development',  
-        'taskUrgency': 'Niedrig', 
-        'taskDescription': 'Diverse Änderungen vorehmen', 
-        'taskCreator': 'Majd', 
-        'createdAt': '1635729160974'
-    },
-    {
-        'taskTitle': 'Datenbank implementieren', 
-        'taskCategory': 'Development',  
-        'taskUrgency': 'Hoch', 
-        'taskDescription': 'Fehler beheben', 
-        'taskCreator': 'Peter', 
-        'createdAt': '1635729335667'
-    },
-    {
-        'taskTitle': 'Projekt neu strukturieren', 
-        'taskCategory': 'Management',  
-        'taskUrgency': 'Normal', 
-        'taskDescription': 'lorem ipsum rv adfvasdgvdy y dg adsrg adfg asdfgadg ad dgadrgadrgasd  asdrfgasdfgdfg sa dg sdfgsd f gsdgsdfg g sdf gh sdfghsf sdfg sdrfgsdg  adfg sdfg sfdgh sfdb', 
-        'taskCreator': 'Toma', 
-        'createdAt': '1635729654321'
-    }
-];
+ let allTasks = [{
+         'taskTitle': 'Css überarbeiten',
+         'taskCategory': 'Development',
+         'taskUrgency': 'Niedrig',
+         'taskDescription': 'Diverse Änderungen vorehmen',
+         'taskCreator': 'Majd',
+         'createdAt': '1635729160974'
+     },
+     {
+         'taskTitle': 'Datenbank implementieren',
+         'taskCategory': 'Development',
+         'taskUrgency': 'Hoch',
+         'taskDescription': 'Fehler beheben',
+         'taskCreator': 'Peter',
+         'createdAt': '1635729335667'
+     },
+     {
+         'taskTitle': 'Projekt neu strukturieren',
+         'taskCategory': 'Management',
+         'taskUrgency': 'Normal',
+         'taskDescription': 'lorem ipsum rv adfvasdgvdy y dg adsrg adfg asdfgadg ad dgadrgadrgasd  asdrfgasdfgdfg sa dg sdfgsd f gsdgsdfg g sdf gh sdfghsf sdfg sdrfgsdg  adfg sdfg sfdgh sfdb',
+         'taskCreator': 'Toma',
+         'createdAt': '1635729654321'
+     }
+ ];
 
-async function init() {
-    await downloadFromServer();
-    allTasks = JSON.parse(backend.getItem('allTasks')) || [];
-}
+ async function init() {
+     await downloadFromServer();
+     allTasks = JSON.parse(backend.getItem('allTasks')) || [];
+ }
 
-function addTask() {
-let taskTitle = document.getElementById('titleInputfield').value;
-let taskCategory = document.getElementById('selectCategory').value;
-let taskDate = document.getElementById('dateInputfield').value;
-let taskUrgency = document.getElementById('selectUrgency').value;
-let taskDescription = document.getElementById('descriptionInputfield').value;
-let taskCreator = document.getElementById('taskCreator').value;
+ function addTask() {
+     let taskTitle = document.getElementById('titleInputfield').value;
+     let taskCategory = document.getElementById('selectCategory').value;
+     let taskDate = document.getElementById('dateInputfield').value;
+     let taskUrgency = document.getElementById('selectUrgency').value;
+     let taskDescription = document.getElementById('descriptionInputfield').value;
+     let taskCreator = document.getElementById('taskCreator').value;
 
-let task = {
-    'taskTitle': taskTitle, 
-    'taskCategory': taskCategory, 
-    'taskDate': taskDate, 
-    'taskUrgency': taskUrgency, 
-    'taskDescription': taskDescription, 
-    'taskCreator': taskCreator, 
-    'createdAt': new Date().getTime()
-};
+     let task = {
+         'taskTitle': taskTitle,
+         'taskCategory': taskCategory,
+         'taskDate': taskDate,
+         'taskUrgency': taskUrgency,
+         'taskDescription': taskDescription,
+         'taskCreator': taskCreator,
+         'createdAt': new Date().getTime()
+     };
 
-allTasks.push(task);
+     allTasks.push(task);
 
-let allTasksAsString = JSON.stringify(allTasks); 
-backend.setItem('allTasks', allTasksAsString);
+     let allTasksAsString = JSON.stringify(allTasks);
+     backend.setItem('allTasks', allTasksAsString);
 
-console.log(allTasks);
-}
+     console.log(allTasks);
+ }
--- js/backlog.js
+++ js/backlog.js
@@ -2,9 +2,10 @@ function showTasks() {
     for (let i = 0; i < allTasks.length; i++) {
 
         const Task = allTasks[i];
+        let urgency = allTasks[i]['taskUrgency'];
 
-        document.getElementById('tasks').innerHTML += `<div class="backlog-box">
-        <div style="width:25%">${Task['taskCreator']}</div><div style="width:10%">${Task['taskCategory']}</div><div style="width:40%">${Task['taskDescription']}</div>
-        </div>`;
+        document.getElementById('tasks').innerHTML += `<div class="backlog-box"><div class="${urgency}"></div>
+        <div style="width:25%">${Task['taskCreator']}</div><div style="width:10%">${Task['taskCategory']}</div><div class="description">${Task['taskDescription']}</div>
+        </div></div>`;
     }
 }
