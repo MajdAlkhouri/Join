725709b9b396f05d591f494f68d96e3ad12cacc4
PeterSteinlesberger <petersteinlesberger@mail.de>
2021-11-12 16:54:44 +0100

    login created

---
+++ js/login.js
@@ -0,0 +1,10 @@
+// $("#login-button").click(function(event){
+ //   event.preventDefault();
+
+ // $('form').fadeOut(500);
+// $('.wrapper').addClass('form-success');
+// });
+
+function login() {
+    document.getElementById('loginCont').classList.addClass('d-none');
+}
