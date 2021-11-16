ae07eccd603d17674e830232329db21421693ef5
Toma Gela <toma.gela@gmail.com>
2021-11-16 12:39:06 +0100

    path correction2

--- js/design.js
+++ js/design.js
@@ -1,27 +1,27 @@
 function changeBg1() {
 
-    document.getElementById('bg').style.backgroundImage = 'url(../img/background_images/beach blue.jpg)';
+    document.getElementById('bg').style.backgroundImage = 'url(./img/background_images/beach blue.jpg)';
 
 
 }
 
 function changeBg2() {
 
-    document.getElementById('bg').style.backgroundImage = 'url(../img/background_images/beach_green.jpg)';
+    document.getElementById('bg').style.backgroundImage = 'url(./img/background_images/beach_green.jpg)';
 
 }
 
 function changeBg3() {
 
-    document.getElementById('bg').style.backgroundImage = 'url(../img/background_images/bridge.jpg)';
+    document.getElementById('bg').style.backgroundImage = 'url(./img/background_images/bridge.jpg)';
 }
 
 function changeBg4() {
 
-    document.getElementById('bg').style.backgroundImage = 'url(../img/background_images/Erde.jpg)';
+    document.getElementById('bg').style.backgroundImage = 'url(./img/background_images/Erde.jpg)';
 }
 
 function changeBg5() {
 
-    document.getElementById('bg').style.backgroundImage = 'url(../img/background_images/monumental rocks.jpg)';
+    document.getElementById('bg').style.backgroundImage = 'url(./img/background_images/monumental_rocks.jpg)';
 }
