8d1f807401e919df55caa0ba0f0ec6ddcbd27012
PeterSteinlesberger <petersteinlesberger@mail.de>
2021-11-29 11:47:23 +0100

    changeCreatorImg-function added

--- js/addTask.js
+++ js/addTask.js
@@ -9,16 +9,13 @@
  function changeCreatorImage() {
      let creator = document.getElementById('taskCreator').value;
      let creatorImg = document.getElementById('creatorImg');
-     if(creator == Majd) {
+     if(creator == 'majd') {
          creatorImg.src = './img/social_icons/facebook-64.ico';
      }
-     if(creator == Toma) {
-        creatorImg.src = './img/social_icons/facebook-64.ico';
+     if(creator == 'toma') {
+        creatorImg.src = './img/icon plus.png';
     }
-    if(creator == Peter) {
+    if(creator == 'peter') {
         creatorImg.src = './img/Peter.jpg';
     }
-
-    console.log('creatorImg.src');
-
  }
