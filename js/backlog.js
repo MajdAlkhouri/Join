0f77dab89f8f424dda48b79e42a06332c4313aec
Toma Gela <toma.gela@gmail.com>
2021-12-23 17:24:08 +0100

    update backlog 231221

--- js/backlog.js
+++ js/backlog.js
@@ -27,7 +27,7 @@ function showTasks() {
         <div class="${urgency}">${urgency}</div>
         </div>
         <div class="column middle d-flex">
-        <div><img src ="${Task['creatorImg']} class="img-profile""></div>
+        <div><img src ="${Task['creatorImg']}" class="img-profile"></div>
         <span>${Task['taskCreator']}</span>
         </div>
         <div class="column middle">${Task['taskCategory']}</div>
